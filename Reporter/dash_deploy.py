aeb70e9cd4ffe15b0bb6393fd57182d09d1395c5
yinsijun <sijun.yin@sagene.com.cn>
2018-09-27 14:58:02 +0800

    CRT temp freezing

--- Reporter/dash_deploy.py
+++ Reporter/dash_deploy.py
@@ -40,16 +40,19 @@ external_css = ["https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normaliz
         "https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
         "//fonts.googleapis.com/css?family=Raleway:400,300,600",
         "/static/KQrXdb.css",
-        "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"]
+        "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+        "/static/bootstrap.min.css",
+        ]
 
 for css in external_css:
     app.css.append_css({"external_url": css})
 
 external_js = [
         "https://code.jquery.com/jquery-3.2.1.min.js",
-        "/static/YaXojL.js"
+        "/static/YaXojL.js",
+        "/static/bootstrap.min.js",
 #        "https://codepen.io/bcd/pen/YaXojL.js"
-]
+        ]
 
 for js in external_js:
     app.scripts.append_script({"external_url": js})
